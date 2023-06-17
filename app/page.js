856a20d8c677e8c109723eb8b22dde3e8ff03811
SimThT <simon.tht@outlook.com>
2023-06-17 23:22:55 +0200

    Setup project

--- app/page.js
+++ app/page.js
@@ -1,95 +1,133 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+// import RentCard from "../components/RentCard";
+// import TopRates from "../components/TopRates";
 
-export default function Home() {
+import styles from "./page.module.css";
+
+import carrousel1 from "/assets/carrousel/1.webp";
+import carrousel2 from "/assets/carrousel/2.webp";
+import carrousel3 from "/assets/carrousel/3.webp";
+import carrousel4 from "/assets/carrousel/4.webp";
+import rents from "/data/rentals.json";
+
+const Home = () => {
+  const carrouselPaths = [carrousel1, carrousel2, carrousel3, carrousel4];
+  const [currentCarrouselIndex, setCurrentCarrouselIndex] = useState(0);
+
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      setCurrentCarrouselIndex(
+        (prevIndex) => (prevIndex + 1) % carrouselPaths.length
+      );
+    }, 5000);
+
+    return () => clearTimeout(timeout);
+  }, [currentCarrouselIndex]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div></div>
+      <div className="carrousel-container">
+        {carrouselPaths.map((carrouselPath, index) => (
+          <div
+            className="carousel-item"
+            style={{
+              backgroundImage: `url(${carrouselPath})`,
+              opacity: `${currentCarrouselIndex === index ? "1" : 0}`,
+            }}
+            key={index}
+          ></div>
+        ))}
+        <div className="carousel-filter">
+          <div className="carousel-text">
+            <h3>Trouver votre location de vacences en Touraine</h3>
+            <h1>Conciergerie Airbnb à Tours et Amboise</h1>
+          </div>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="container prestations-section">
+        <h3>Propriétaire et désireux de déléguer la gestion de votre bien ?</h3>
+        <h3>De l'accueil au ménage, nous gérons tout à votre place !</h3>
+        <div className="prestations-btn">
+          <Link to="/contact" className="btn">
+            Nos prestations en détails
+          </Link>
+        </div>
+        <div className="prestation-cards">
+          <div className="prestation-card">
+            <h3>Ménage premium</h3>
+            <p>
+              Par notre partenaire de confiance (respect des normes d'hygiènes
+              COVID-19)
+            </p>
+          </div>
+          <div className="prestation-card">
+            <h3>Blanchisserie haut de gamme</h3>
+            <p>
+              En gérant l'ensemble du linge de maison (draps, serviettes,
+              torchons)
+            </p>
+          </div>
+          <div className="prestation-card">
+            <h3>Accueil personnalisé 24/7</h3>
+            <p>
+              A toute heure du jour en personne et de la nuit grâce à nos
+              boitiers à clés
+            </p>
+          </div>
+          <div className="prestation-card">
+            <h3>Création des annonces</h3>
+            <p>
+              Photos professionnelles, description attrayante et personnalisée
+            </p>
+          </div>
+          <div className="prestation-card">
+            <h3>Optimisation des prix</h3>
+            <p>
+              Sur toutes les plateformes de locations courtes durées grâce a
+              notre logiciel de management
+            </p>
+          </div>
+          <div className="prestation-card">
+            <h3>Entretien des propriétés</h3>
+            <p>
+              Fournitures des consommables pour la 1ére nuit et gestion des
+              incidents
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="location-section">
+        <div className="container">
+          <div className="location-section-top">
+            <div>
+              <h2>Nos Locations</h2>
+              <p>Découvrez notre selection du moment</p>
+            </div>
+            <a
+              href="https://www.airbnb.fr/users/479186120/listings"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Découvrir toutes nos locations
+            </a>
+          </div>
+          <div className="rent-selection">
+            {rents.map((rent) => (
+              <div className="selection-card-container" key={rent.id}>
+                <RentCard rent={rent} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="top-rate">
+        <TopRates />
+      </section>
     </main>
-  )
-}
+  );
+};
+
+export default Home;
